db7e97ce25f4a7889318c22f8c13f1b27a527fc7
Ken-18I <kbazant@uni.pe>
2022-08-04 01:43:20 -0500

    Client

--- src/main/java/galaga_client/Client.java
+++ src/main/java/galaga_client/Client.java
@@ -1,4 +1,14 @@
 package galaga_client;
-
+import com.galaga_game.Galaga;
 public class Client {
+    public static void main(String[] args) {
+        //GalagaGUI galagaGUI = new GalagaGUI();
+        Galaga galaga = new Galaga();
+        /*new Thread(new TCPClient("192.168.0.27", new TCPClient.OnMessageReceived() {
+            @Override
+            public void messageReceived(String message) {
+                System.out.println(message);
+            }
+        })).start();*/
+    }
 }
