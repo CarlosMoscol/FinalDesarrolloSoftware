bad3b9974cb625362e349557a119c82fbffe66fc
Ken-18I <kbazant@uni.pe>
2022-08-06 04:26:05 -0500

    GalagaTest 2.0

--- src/main/java/galaga_game/GalagaTest.java
+++ src/main/java/galaga_game/GalagaTest.java
@@ -8,13 +8,13 @@ class GalagaTest {
 
     @Test
     void init() {
-        System.out.println("Testing initialization of all galaga threads...");
+        System.out.println("Probando la inicialización de todos los hilos galaga...");
         galaga.init();
     }
 
     @Test
     void startClient() throws InterruptedException {
-        System.out.println("Testing the initialization of a client server...");
+        System.out.println("Probando la inicialización de un servidor cliente...");
         String IP = "192.0.0.1";
         int PORT = 8008;
         galaga.startClient(IP,PORT);
@@ -22,14 +22,14 @@ class GalagaTest {
 
     @Test
     void setID() {
-        System.out.println("Testing the assignment of the ID to the Galaga object...");
+        System.out.println("Probando la asignación de la ID al objeto Galaga...");
         int ID = 404;
         galaga.setID(ID);
     }
 
     @Test
     void received() throws InterruptedException {
-        System.out.println("testing if the command is received from the server...");
+        System.out.println("Probando si el comando se recibe del servidor...");
         String server = "8080";
         galaga.received(server);
     }
